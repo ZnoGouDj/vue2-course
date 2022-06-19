d43027ebd9315d51567d2131b32a7a03e7772fd3
ZnoGouDj <znogoud3@gmail.com>
2022-06-20 00:35:34 +0200

    PhotoDialog

--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -2,29 +2,28 @@
   <v-container>
     <PhotoForm @addPhoto="addPhoto" />
     <v-row>
-      <Photo v-for="photo in photos" :photo="photo" :key="photo.id" />
+      <Photo v-for="photo in photos" :photo="photo" :key="photo.id" @openPhoto="openPhoto" />
     </v-row>
+    <PhotoDialog :photo="currentPhoto" v-model="dialogVisible" />
   </v-container>
 </template>
 
 <script>
 import Photo from '@/components/photo/Photo';
 import PhotoForm from '@/components/photo/PhotoForm.vue';
+import PhotoDialog from '@/components/photo/PhotoDialog.vue';
 export default {
   components: {
     Photo,
     PhotoForm,
+    PhotoDialog,
   },
   data: () => ({
-    photos: [
-      { id: 1, title: 'Photo 1' },
-      { id: 2, title: 'Photo 2' },
-      { id: 3, title: 'Photo 3' },
-      { id: 4, title: 'Photo 4' },
-      { id: 5, title: 'Photo 5' },
-      { id: 6, title: 'Photo 6' },
-    ],
+    photos: [],
+    currentPhoto: {},
+    dialogVisible: false,
   }),
+
   mounted() {
     this.fetchTodo();
   },
@@ -36,6 +35,10 @@ export default {
     },
     addPhoto(photo) {
       this.photos.push(photo);
+    },
+    openPhoto(photo) {
+      this.currentPhoto = photo;
+      this.dialogVisible = true;
     },
   },
 };
